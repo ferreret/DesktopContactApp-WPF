e1c9918e407717211e7123abecb9d5ee22ea0303
Nicolás Barceló <ferreret@gmail.com>
2024-04-25 13:02:00 +0200

    Add project files.

---
+++ DesktopContactApp/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using DesktopContactApp.Classes;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopContactApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ReadDatabase();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NewContactWindow newContactWindow = new();
+            newContactWindow.ShowDialog();
+
+            ReadDatabase();
+        }
+
+        void ReadDatabase()
+        {
+            List<Contact> contacts;
+
+            using (SQLite.SQLiteConnection connection = new(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                contacts = connection.Table<Contact>().ToList();
+            }
+
+            if (contacts != null)
+            {
+                contactsListView.ItemsSource = contacts;             
+            }            
+        }
+    }
+}
---
+++ DesktopContactApp/NewContactWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using DesktopContactApp.Classes;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesktopContactApp
+{
+    /// <summary>
+    /// Interaction logic for NewContactWindow.xaml
+    /// </summary>
+    public partial class NewContactWindow : Window
+    {
+        public NewContactWindow() => InitializeComponent();
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Save contact
+            Contact contact = new()
+            {
+                Name = nameTextBox.Text,
+                Email = emailTextBox.Text,
+                PhoneNumber = phoneTextBox.Text
+            };
+
+            using (SQLiteConnection connection = new(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                connection.Insert(contact);
+            }
+
+            Close();
+        }
+    }
+}
